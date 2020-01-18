9f9d56d88f18b87e2ed6d9efcc773f2d1ee9cae4
Shannon Ervin <sjervin07@gmail.com>
2020-01-18 12:07:47 -0600

    Completed API routes, re-added db.json

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,23 +1,44 @@
 //Sets up database json
-var notes = require("../db/db");
+// var notes = require("../db/db");
+
+var fs = require("fs")
+var path = require("path")
 
 module.exports = function(app) {
 
     app.get("/api/notes", function(req, res) {
-        res.json(notes);
+        res.json(getdatabase());
+        
     });
 
 
     app.post("/api/notes", function(req, res) {
           
+        notes.push(req.body);
+        console.log(notes)
       });
 
-    app.delete("/api/notes", function(req, res) {
+    app.delete("/api/notes/:id", function(req, res) {
 
+        var database = getdatabase();
+
+        // Empty out the arrays of data
+        database = database.filter (note => note.id != req.params.id)  
+
+        //
+        
+        res.json(database);
+        savedatbase(database);
     });
+
+          
 }
 
+//Using getdatbase and savedatabase to save and rewrite notes 
+function getdatabase() {
+    return JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json")));
+}
 
-
-
-//assign something to hold text...like text.title, notes.title
+function savedatbase(data) {
+    return fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify(data));
+}
--- server.js
+++ server.js
@@ -11,10 +11,12 @@ var PORT = process.env.PORT || 8080;
 //Creating express app
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"))
 
 //Linking html and api routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
+
 
 //Listener
 app.listen(PORT, function() {
