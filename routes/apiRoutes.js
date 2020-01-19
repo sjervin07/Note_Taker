c21db197e66c22aa69728485df1aa2806dd218f5
Shannon Ervin <sjervin07@gmail.com>
2020-01-18 22:28:08 -0600

    Final finished to API routes, posting and deleting work correctly

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,6 +3,7 @@
 
 var fs = require("fs")
 var path = require("path")
+let notesArray = getdatabase();
 
 module.exports = function(app) {
 
@@ -11,34 +12,65 @@ module.exports = function(app) {
         
     });
 
-
     app.post("/api/notes", function(req, res) {
-          
-        notes.push(req.body);
-        console.log(notes)
+
+        const newNote = req.body;
+        newNote.id = Math.floor(Math.random() * 10000);
+        notesArray.push(newNote)
+        //req.body = addedNote;
+        //notesArray.push(req.body);
+        //notesArray.push({ title: "example note", text: "random text" });
+        savedatabase(notesArray);
+
       });
 
     app.delete("/api/notes/:id", function(req, res) {
 
-        var database = getdatabase();
+        //var database = getdatabase();
 
         // Empty out the arrays of data
-        database = database.filter (note => note.id != req.params.id)  
+        notesArray = notesArray.filter (note => note.id != req.params.id)  
 
-        //
-        
-        res.json(database);
-        savedatbase(database);
+        res.json(notesArray);
+        savedatabase(notesArray);
     });
 
-          
 }
 
 //Using getdatbase and savedatabase to save and rewrite notes 
-function getdatabase() {
-    return JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json")));
-}
 
-function savedatbase(data) {
-    return fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify(data));
-}
+function getdatabase() {
+    const json = fs.readFileSync(path.join(__dirname, "../db/db.json"));
+    return JSON.parse(json);
+  }
+  
+  function savedatabase(newData) {
+    return fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify(newData))
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // /* /* this returns a copy of database that can be modified */
+// /* loads the .json file and returns a copied array[] */
+//   /* this will write over the .json file with whatever you pass to this function */
+//   to use this all you need to do is get rid of that require and do
+//   /* example: adding note to database */
+//   // 1) make a copy of the database (from .json)
+//   const notesArray = getDatabase();
+//   // 2) modify our copy of the .json array
+//   notesArray.push({ title: "example note", text: "here is some text :D" });
+//   // 3) write the modified copy back to the .json file (this will replace it)
+//   saveDatabase(notesArray);
+//   /* and done!! */
+//  */
